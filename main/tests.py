f7e8cdf35df4adedeb3e63fb6613624c710b3d30
Francisco Kahil <codexico@gmail.com>
2021-04-12 15:53:06 +0300

    some updates for testing

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,4 @@
-from otree.api import Currency as c, currency_range, Submission,SubmissionMustFail
+from otree.api import Currency as c, currency_range, Submission, SubmissionMustFail
 from .pages import *
 from qualifier.generic_tests import PlayerBot as Bot
 import random
@@ -8,14 +8,25 @@ import numpy as np
 
 class PlayerBot(Bot):
     def play_round(self):
-        if self.round_number==1:
+        if self.round_number == 1:
             yield RoleAnnouncement
+            yield WorkerAnnounceNextPeriod,
+            yield RoleAnnouncement,
+            yield BackgroundStage2,
+            yield WorkerAbilityExplained,
+            yield ShockExplained,
+            yield RealizedOutputExplained,
+            yield WorkerBonusesExplained,
+            yield ManagerBonusesExplained,
+            yield BonusDistributionExplained,
+            yield PaymentExplained,
+            yield Quiz,
         for i in range(random.randint(3, 5)):
             self.call_method(RET)
         yield Submission(WorkingRET, check_html=False)
         n = 3
         m = self.group.total_bonus
-        wrong_bonuses = np.random.multinomial(m+100, np.ones(n) / n)
+        wrong_bonuses = np.random.multinomial(m + 100, np.ones(n) / n)
         bonuses = np.random.multinomial(m, np.ones(n) / n)
         keys = [f'bonus_{i}' for i in Constants.subtypes]
         if self.player.role() == Role.manager:

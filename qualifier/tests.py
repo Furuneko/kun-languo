426a981802db72c11db4e6654ff8419eb33b8308
Francisco Kahil <codexico@gmail.com>
2021-04-12 15:52:17 +0300

    some updates for testing

--- qualifier/tests.py
+++ qualifier/tests.py
@@ -4,11 +4,16 @@ from .generic_tests import PlayerBot as Bot
 import random
 from string import ascii_lowercase
 
+
 class PlayerBot(Bot):
     def play_round(self):
-        for i in range(random.randint(1,5)):
+        yield ExplainingDecodingTask,
+        for i in range(random.randint(1, 5)):
             self.call_method(Practice)
         yield Submission(Practice, check_html=False)
-        for i in range(random.randint(5,15)):
+        yield PracticeRETFeedback
+        yield RETIntro
+        for i in range(random.randint(5, 15)):
             self.call_method(RET)
         yield Submission(RET, check_html=False)
+        yield PerformanceRETFeedback
